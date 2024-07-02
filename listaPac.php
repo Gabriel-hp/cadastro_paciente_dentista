0932107c4b2f06b381449aa80b30e788fbf2c930
Gabriel Dias <72035836+Gabriel-hp@users.noreply.github.com>
2024-07-01 21:49:05 -0400

    Update listaPac.php

--- listaPac.php
+++ listaPac.php
@@ -13,7 +13,7 @@
     include __DIR__.'./app/includes/header.php';
     include __DIR__.'./app/includes/incial.php';
     include __DIR__.'./app/includes/listarUsu.php'; 
-    include __DIR__.'./app/includes/footer.php';
+    include __DIR__.'./app/includes/footer.';
 
 
 
